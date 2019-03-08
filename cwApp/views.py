fada61330ee79593ec25691897fd5aab83f8635b
Litterial <yungice12@gmail.com>
2019-03-08 12:48:25 -0600

    comments

--- cwApp/views.py
+++ cwApp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def index(request): #sends user to page 1
     return render(request,'cwApp/index.html')
-def p2(request):
+def p2(request): #sends user to page 2
     return render(request,'cwApp/page2.html')
-def p3(request):
+def p3(request):#sends user to page 3
     return render(request,'cwApp/page3.html')
